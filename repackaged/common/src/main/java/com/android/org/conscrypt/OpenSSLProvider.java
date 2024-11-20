5ae7b5c2f272365d13c9ff3ba0c7a682375dc1d8
Miguel Aranda <miguelaranda@google.com>
2024-11-20 16:40:07 +0000

    Revert^3 "Add support for enabling/disabling TLS v1.0 and 1.1 in Conscrypt."
    This reverts commit f7b7007b88689ac93b57a8bec30ec67c122611de.
    
    Reason for revert: failures like https://android-build.corp.google.com/test_investigate/invocation/I62400010337601910/test/TR05829522654933077/?itab=tests&ttab=artifacts&status=none
    
    Change-Id: Ic5e54b71cb4516286be0bb0497694272167de5b9

--- repackaged/common/src/main/java/com/android/org/conscrypt/OpenSSLProvider.java
+++ repackaged/common/src/main/java/com/android/org/conscrypt/OpenSSLProvider.java
@@ -58,29 +58,17 @@ public final class OpenSSLProvider extends Provider {
 
     @SuppressWarnings("deprecation")
     public OpenSSLProvider(String providerName) {
-        this(providerName, Platform.provideTrustManagerByDefault(), "TLSv1.3",
-            Platform.DEPRECATED_TLS_V1, Platform.ENABLED_TLS_V1);
+        this(providerName, Platform.provideTrustManagerByDefault(), "TLSv1.3");
     }
 
-    OpenSSLProvider(String providerName, boolean includeTrustManager,
-            String defaultTlsProtocol) {
-        this(providerName, includeTrustManager, defaultTlsProtocol,
-            Platform.DEPRECATED_TLS_V1, Platform.ENABLED_TLS_V1);
-    }
-
-    OpenSSLProvider(String providerName, boolean includeTrustManager,
-            String defaultTlsProtocol, boolean deprecatedTlsV1,
-            boolean enabledTlsV1) {
+    OpenSSLProvider(String providerName, boolean includeTrustManager, String defaultTlsProtocol) {
         super(providerName, 1.0, "Android's OpenSSL-backed security provider");
 
         // Ensure that the native library has been loaded.
         NativeCrypto.checkAvailability();
 
-        if (!deprecatedTlsV1 && !enabledTlsV1) {
-            throw new IllegalArgumentException("TLSv1 is not deprecated and cannot be disabled.");
-        }
         // Make sure the platform is initialized.
-        Platform.setup(deprecatedTlsV1, enabledTlsV1);
+        Platform.setup();
 
         /* === SSL Contexts === */
         String classOpenSSLContextImpl = PREFIX + "OpenSSLContextImpl";
