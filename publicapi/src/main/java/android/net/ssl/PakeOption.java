Tharow-Services/conscrypt

Rework the SPAKE2+ API
Provide a more flexible API that allows implementation and usage of different
PAKE protocols.

Bug: 382233100
Test: atest CtsLibcoreTestCases:android.net.ssl.PakeEndpointsTest
Test: atest CtsLibcoreTestCases:android.net.ssl.PakeOptionTest
Test: atest CtsLibcoreTestCases:android.net.ssl.PakeClientKeyManagerParametersTest
Test: atest CtsLibcoreTestCases:android.net.ssl.PakeServerKeyManagerParametersTest
Change-Id: I0452eb52e461370ff22d4db0951ff3995c55ac0a

publicapi/src/main/java/android/net/ssl/PakeOption.java
@@ -1,0 +1,169 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.net.ssl;
+
+import android.annotation.FlaggedApi;
+import android.annotation.SystemApi;
+
+import libcore.util.NonNull;
+import libcore.util.Nullable;
+
+import java.security.InvalidParameterException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * An class representing a PAKE (Password Authenticated Key Exchange)
+ * option for TLS connections.
+ *
+ * <p>Instances of this class are immutable. Use the {@link Builder} to create
+ * instances.</p>
+ *
+ * @hide
+ */
+@SystemApi
+@FlaggedApi(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+public final class PakeOption {
+    /**
+     * The name of the PAKE algorithm.
+     */
+    private final String name; // For now "SPAKE2PLUS_PRERELEASE" is suported
+
+    /**
+     * A map containing the message components for the PAKE exchange.
+     *
+     * <p>The keys are strings representing the component names (e.g., "password",
+     * "w0", "w1"). The values are byte arrays containing the component data.</p>
+     */
+    private final Map<String, byte[]> messageComponents;
+
+    private PakeOption(String name, Map<String, byte[]> messageComponents) {
+        this.name = name;
+        this.messageComponents = Collections.unmodifiableMap(new HashMap<>(messageComponents));
+    }
+
+    /**
+     * Returns the name of the PAKE algorithm.
+     *
+     * @return The name of the PAKE algorithm.
+     */
+    public @NonNull String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the message component with the given key.
+     *
+     * @param key The name of the component.
+     * @return The component data, or {@code null} if no component with the given
+     *         key exists.
+     */
+    public @Nullable byte[] getMessageComponent(@NonNull String key) {
+        return messageComponents.get(key);
+    }
+
+    /**
+     * A builder for creating {@link PakeOption} instances.
+     *
+     * @hide
+     */
+    @SystemApi
+    @FlaggedApi(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public static final class Builder {
+        private String name;
+        private Map<String, byte[]> messageComponents = new HashMap<>();
+
+        /**
+         * Constructor for the builder.
+         *
+         * @param name The name of the PAKE algorithm.
+         * @throws InvalidParameterException If the name is invalid.
+         */
+        public Builder(@NonNull String name) {
+            if (name == null || name.isEmpty()) {
+                throw new InvalidParameterException("Name cannot be null or empty.");
+            }
+            this.name = name;
+        }
+
+        /**
+         * Adds a message component.
+         *
+         * @param key The name of the component.
+         * @param value The component data.
+         * @return This builder.
+         * @throws InvalidParameterException If the key is invalid.
+         */
+        public @NonNull Builder addMessageComponent(@NonNull String key, @Nullable byte[] value) {
+            if (key == null || key.isEmpty()) {
+                throw new InvalidParameterException("Key cannot be null or empty.");
+            }
+            messageComponents.put(key, value);
+            return this;
+        }
+
+        /**
+         * Builds a new {@link PakeOption} instance.
+         *
+         * <p>This method performs validation to ensure that the message components
+         * are consistent with the PAKE algorithm.</p>
+         *
+         * @return A new {@link PakeOption} instance.
+         * @throws InvalidParameterException If the message components are invalid.
+         */
+        public @NonNull PakeOption build() {
+            if (messageComponents.isEmpty()) {
+                throw new InvalidParameterException("Message components cannot be empty.");
+            }
+            if (name.equals("SPAKE2PLUS_PRERELEASE")) {
+                validateSpake2PlusComponents();
+            }
+
+            return new PakeOption(name, messageComponents);
+        }
+
+        private void validateSpake2PlusComponents() {
+            // For SPAKE2+ one of the following is present exclusively:
+            //  - password
+            //  - w0 and w1 (for Client)
+            //  - w0 and registration_record (for Server)
+            if (messageComponents.containsKey("password")) {
+                if (messageComponents.containsKey("w0") || messageComponents.containsKey("w1")
+                        || messageComponents.containsKey("registration_record")) {
+                    throw new InvalidParameterException(
+                            "For SPAKE2+, 'password' must be exclusive.");
+                }
+            } else if (messageComponents.containsKey("w0")) {
+                if (!messageComponents.containsKey("w1")
+                        && !messageComponents.containsKey("registration_record")) {
+                    throw new InvalidParameterException("For SPAKE2+, 'w0' must be present with "
+                            + "either 'w1' or 'registration_record'.");
+                }
+                if (messageComponents.containsKey("w1")
+                        && messageComponents.containsKey("registration_record")) {
+                    throw new InvalidParameterException(
+                            "For SPAKE2+, 'w1' and 'registration_record' cannot both be present.");
+                }
+            } else {
+                throw new InvalidParameterException(
+                        "For SPAKE2+, one of 'password', 'w0' with 'w1', or 'w0' with "
+                        + "'registration_record' must be present.");
+            }
+        }
+    }
+}

publicapi/src/test/java/android/net/ssl/PakeOptionTest.java
@@ -1,0 +1,153 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.net.ssl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import android.platform.test.annotations.RequiresFlagsEnabled;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.security.InvalidParameterException;
+
+@RunWith(JUnit4.class)
+public class PakeOptionTest {
+    @Test
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_valid() {
+        PakeOption option = new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                                    .addMessageComponent("password", new byte[] {1, 2, 3})
+                                    .build();
+        assertEquals("SPAKE2PLUS_PRERELEASE", option.getName());
+        assertNotNull(option.getMessageComponent("password"));
+    }
+
+    @Test
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_validWithW0W1() {
+        PakeOption option = new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                                    .addMessageComponent("w0", new byte[] {1, 2, 3})
+                                    .addMessageComponent("w1", new byte[] {4, 5, 6})
+                                    .build();
+        assertEquals("SPAKE2PLUS_PRERELEASE", option.getName());
+        assertNotNull(option.getMessageComponent("w0"));
+        assertNotNull(option.getMessageComponent("w1"));
+    }
+
+    @Test
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_validWithW0RegistrationRecord() {
+        PakeOption option =
+                new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                        .addMessageComponent("w0", new byte[] {1, 2, 3})
+                        .addMessageComponent("registration_record", new byte[] {4, 5, 6})
+                        .build();
+        assertEquals("SPAKE2PLUS_PRERELEASE", option.getName());
+        assertNotNull(option.getMessageComponent("w0"));
+        assertNotNull(option.getMessageComponent("registration_record"));
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_invalidName() {
+        new PakeOption.Builder(null);
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_emptyName() {
+        new PakeOption.Builder("");
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_noComponents() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE").build();
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_invalidKey() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                .addMessageComponent(null, new byte[] {1, 2, 3});
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_emptyKey() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                .addMessageComponent("", new byte[] {1, 2, 3});
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_invalidSpake2Plus_passwordWithW0() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                .addMessageComponent("password", new byte[] {1, 2, 3})
+                .addMessageComponent("w0", new byte[] {4, 5, 6})
+                .build();
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_invalidSpake2Plus_passwordWithW1() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                .addMessageComponent("password", new byte[] {1, 2, 3})
+                .addMessageComponent("w1", new byte[] {4, 5, 6})
+                .build();
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_invalidSpake2Plus_passwordWithRegistrationRecord() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                .addMessageComponent("password", new byte[] {1, 2, 3})
+                .addMessageComponent("registration_record", new byte[] {4, 5, 6})
+                .build();
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_invalidSpake2Plus_w0WithoutW1OrRegistrationRecord() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                .addMessageComponent("w0", new byte[] {1, 2, 3})
+                .build();
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_invalidSpake2Plus_w0WithW1AndRegistrationRecord() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                .addMessageComponent("w0", new byte[] {1, 2, 3})
+                .addMessageComponent("w1", new byte[] {4, 5, 6})
+                .addMessageComponent("registration_record", new byte[] {7, 8, 9})
+                .build();
+    }
+
+    @Test
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testGetMessageComponent_nonExistingKey() {
+        PakeOption option = new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                                    .addMessageComponent("password", new byte[] {1, 2, 3})
+                                    .build();
+        assertNull(option.getMessageComponent("non_existing_key"));
+    }
+}

msft-mirror-aosp/platform.external.conscrypt

publicapi/src/main/java/android/net/ssl/PakeOption.java
@@ -1,0 +1,169 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.net.ssl;
+
+import android.annotation.FlaggedApi;
+import android.annotation.SystemApi;
+
+import libcore.util.NonNull;
+import libcore.util.Nullable;
+
+import java.security.InvalidParameterException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * An class representing a PAKE (Password Authenticated Key Exchange)
+ * option for TLS connections.
+ *
+ * <p>Instances of this class are immutable. Use the {@link Builder} to create
+ * instances.</p>
+ *
+ * @hide
+ */
+@SystemApi
+@FlaggedApi(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+public final class PakeOption {
+    /**
+     * The algorithm of the PAKE algorithm.
+     */
+    private final String algorithm; // For now "SPAKE2PLUS_PRERELEASE" is suported
+
+    /**
+     * A map containing the message components for the PAKE exchange.
+     *
+     * <p>The keys are strings representing the component algorithms (e.g., "password",
+     * "w0", "w1"). The values are byte arrays containing the component data.</p>
+     */
+    private final Map<String, byte[]> messageComponents;
+
+    private PakeOption(String algorithm, Map<String, byte[]> messageComponents) {
+        this.algorithm = algorithm;
+        this.messageComponents = Collections.unmodifiableMap(new HashMap<>(messageComponents));
+    }
+
+    /**
+     * Returns the algorithm of the PAKE algorithm.
+     *
+     * @return The algorithm of the PAKE algorithm.
+     */
+    public @NonNull String getAlgorithm() {
+        return algorithm;
+    }
+
+    /**
+     * Returns the message component with the given key.
+     *
+     * @param key The algorithm of the component.
+     * @return The component data, or {@code null} if no component with the given
+     *         key exists.
+     */
+    public @Nullable byte[] getMessageComponent(@NonNull String key) {
+        return messageComponents.get(key);
+    }
+
+    /**
+     * A builder for creating {@link PakeOption} instances.
+     *
+     * @hide
+     */
+    @SystemApi
+    @FlaggedApi(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public static final class Builder {
+        private String algorithm;
+        private Map<String, byte[]> messageComponents = new HashMap<>();
+
+        /**
+         * Constructor for the builder.
+         *
+         * @param algorithm The algorithm of the PAKE algorithm.
+         * @throws InvalidParameterException If the algorithm is invalid.
+         */
+        public Builder(@NonNull String algorithm) {
+            if (algorithm == null || algorithm.isEmpty()) {
+                throw new InvalidParameterException("Algorithm cannot be null or empty.");
+            }
+            this.algorithm = algorithm;
+        }
+
+        /**
+         * Adds a message component.
+         *
+         * @param key The algorithm of the component.
+         * @param value The component data.
+         * @return This builder.
+         * @throws InvalidParameterException If the key is invalid.
+         */
+        public @NonNull Builder addMessageComponent(@NonNull String key, @Nullable byte[] value) {
+            if (key == null || key.isEmpty()) {
+                throw new InvalidParameterException("Key cannot be null or empty.");
+            }
+            messageComponents.put(key, value);
+            return this;
+        }
+
+        /**
+         * Builds a new {@link PakeOption} instance.
+         *
+         * <p>This method performs validation to ensure that the message components
+         * are consistent with the PAKE algorithm.</p>
+         *
+         * @return A new {@link PakeOption} instance.
+         * @throws InvalidParameterException If the message components are invalid.
+         */
+        public @NonNull PakeOption build() {
+            if (messageComponents.isEmpty()) {
+                throw new InvalidParameterException("Message components cannot be empty.");
+            }
+            if (algorithm.equals("SPAKE2PLUS_PRERELEASE")) {
+                validateSpake2PlusComponents();
+            }
+
+            return new PakeOption(algorithm, messageComponents);
+        }
+
+        private void validateSpake2PlusComponents() {
+            // For SPAKE2+ one of the following is present exclusively:
+            //  - password
+            //  - w0 and w1 (for Client)
+            //  - w0 and registration_record (for Server)
+            if (messageComponents.containsKey("password")) {
+                if (messageComponents.containsKey("w0") || messageComponents.containsKey("w1")
+                        || messageComponents.containsKey("registration_record")) {
+                    throw new InvalidParameterException(
+                            "For SPAKE2+, 'password' must be exclusive.");
+                }
+            } else if (messageComponents.containsKey("w0")) {
+                if (!messageComponents.containsKey("w1")
+                        && !messageComponents.containsKey("registration_record")) {
+                    throw new InvalidParameterException("For SPAKE2+, 'w0' must be present with "
+                            + "either 'w1' or 'registration_record'.");
+                }
+                if (messageComponents.containsKey("w1")
+                        && messageComponents.containsKey("registration_record")) {
+                    throw new InvalidParameterException(
+                            "For SPAKE2+, 'w1' and 'registration_record' cannot both be present.");
+                }
+            } else {
+                throw new InvalidParameterException(
+                        "For SPAKE2+, one of 'password', 'w0' with 'w1', or 'w0' with "
+                        + "'registration_record' must be present.");
+            }
+        }
+    }
+}

publicapi/src/test/java/android/net/ssl/PakeOptionTest.java
@@ -1,0 +1,153 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.net.ssl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import android.platform.test.annotations.RequiresFlagsEnabled;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.security.InvalidParameterException;
+
+@RunWith(JUnit4.class)
+public class PakeOptionTest {
+    @Test
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_valid() {
+        PakeOption option = new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                                    .addMessageComponent("password", new byte[] {1, 2, 3})
+                                    .build();
+        assertEquals("SPAKE2PLUS_PRERELEASE", option.getAlgorithm());
+        assertNotNull(option.getMessageComponent("password"));
+    }
+
+    @Test
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_validWithW0W1() {
+        PakeOption option = new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                                    .addMessageComponent("w0", new byte[] {1, 2, 3})
+                                    .addMessageComponent("w1", new byte[] {4, 5, 6})
+                                    .build();
+        assertEquals("SPAKE2PLUS_PRERELEASE", option.getAlgorithm());
+        assertNotNull(option.getMessageComponent("w0"));
+        assertNotNull(option.getMessageComponent("w1"));
+    }
+
+    @Test
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_validWithW0RegistrationRecord() {
+        PakeOption option =
+                new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                        .addMessageComponent("w0", new byte[] {1, 2, 3})
+                        .addMessageComponent("registration_record", new byte[] {4, 5, 6})
+                        .build();
+        assertEquals("SPAKE2PLUS_PRERELEASE", option.getAlgorithm());
+        assertNotNull(option.getMessageComponent("w0"));
+        assertNotNull(option.getMessageComponent("registration_record"));
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_invalidAlgorithm() {
+        new PakeOption.Builder(null);
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_emptyAlgorithm() {
+        new PakeOption.Builder("");
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_noComponents() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE").build();
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_invalidKey() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                .addMessageComponent(null, new byte[] {1, 2, 3});
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_emptyKey() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                .addMessageComponent("", new byte[] {1, 2, 3});
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_invalidSpake2Plus_passwordWithW0() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                .addMessageComponent("password", new byte[] {1, 2, 3})
+                .addMessageComponent("w0", new byte[] {4, 5, 6})
+                .build();
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_invalidSpake2Plus_passwordWithW1() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                .addMessageComponent("password", new byte[] {1, 2, 3})
+                .addMessageComponent("w1", new byte[] {4, 5, 6})
+                .build();
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_invalidSpake2Plus_passwordWithRegistrationRecord() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                .addMessageComponent("password", new byte[] {1, 2, 3})
+                .addMessageComponent("registration_record", new byte[] {4, 5, 6})
+                .build();
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_invalidSpake2Plus_w0WithoutW1OrRegistrationRecord() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                .addMessageComponent("w0", new byte[] {1, 2, 3})
+                .build();
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testBuilder_invalidSpake2Plus_w0WithW1AndRegistrationRecord() {
+        new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                .addMessageComponent("w0", new byte[] {1, 2, 3})
+                .addMessageComponent("w1", new byte[] {4, 5, 6})
+                .addMessageComponent("registration_record", new byte[] {7, 8, 9})
+                .build();
+    }
+
+    @Test
+    @RequiresFlagsEnabled(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+    public void testGetMessageComponent_nonExistingKey() {
+        PakeOption option = new PakeOption.Builder("SPAKE2PLUS_PRERELEASE")
+                                    .addMessageComponent("password", new byte[] {1, 2, 3})
+                                    .build();
+        assertNull(option.getMessageComponent("non_existing_key"));
+    }
+}