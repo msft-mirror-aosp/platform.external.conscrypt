c7efc39ba3f2d2accf12a4a235ff60be700a6d97
Sorin Basca <sorinbasca@google.com>
2025-01-20 14:46:56 +0000

    Rename registration_record to L for SPAKE2+ parameters
    Bug: 382233100
    Test: atest CtsLibcoreTestCases:android.net.ssl
    Change-Id: I6f4965a08ed020739659fb1869699c91aa4ff31e

--- common/src/main/java/org/conscrypt/Spake2PlusKeyManager.java
+++ common/src/main/java/org/conscrypt/Spake2PlusKeyManager.java
@@ -30,22 +30,21 @@ public class Spake2PlusKeyManager implements KeyManager {
   private final byte[] password;
   private final byte[] w0;
   private final byte[] w1;
-  private final byte[] registrationRecord;
+  private final byte[] l;
   private final byte[] idProver;
   private final byte[] idVerifier;
   private final boolean isClient;
 
-  Spake2PlusKeyManager(byte[] context, byte[] password,
-      byte[] w0, byte[] w1, byte[] registrationRecord,
-      byte[] idProver, byte[] idVerifier, boolean isClient) {
-    this.context = context;
-    this.password = password;
-    this.w0 = w0;
-    this.w1 = w1;
-    this.registrationRecord = registrationRecord;
-    this.idProver = idProver;
-    this.idVerifier = idVerifier;
-    this.isClient = isClient;
+  Spake2PlusKeyManager(byte[] context, byte[] password, byte[] w0, byte[] w1, byte[] l,
+          byte[] idProver, byte[] idVerifier, boolean isClient) {
+      this.context = context;
+      this.password = password;
+      this.w0 = w0;
+      this.w1 = w1;
+      this.l = l;
+      this.idProver = idProver;
+      this.idVerifier = idVerifier;
+      this.isClient = isClient;
   }
 
   public String chooseEngineAlias(String keyType,
@@ -66,16 +65,16 @@ public class Spake2PlusKeyManager implements KeyManager {
     return password;
   }
 
-  public byte[] getw0() {
-    return w0;
+  public byte[] getW0() {
+      return w0;
   }
 
-  public byte[] getw1() {
-    return w1;
+  public byte[] getW1() {
+      return w1;
   }
 
-  public byte[] getRegistrationRecord() {
-    return registrationRecord;
+  public byte[] getL() {
+      return l;
   }
 
   public byte[] getIdProver() {
